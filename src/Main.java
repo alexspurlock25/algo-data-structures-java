2ec1fe381fec3c8b47b3046a9f4f0a9afd181d40
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-04-28 17:21:34 -0400

    Update Main.java
    - Inserting random numbers into num for testing sorting algos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Sort sort = new Sort();
+        int[] sorted = sort.mergeSort(randomNumbers());
+
+        for (int num : sorted) {
+            System.out.println(num);
+        }
+    }
+    private static int[] randomNumbers() {
+        Random rand = new Random();
+        int[] randomNumbers = new int[50];
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = rand.nextInt(200);
+        }
+        return randomNumbers;
     }
 }
