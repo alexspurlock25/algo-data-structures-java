beed4424ee4710b6023160f5743348e5df65d9f3
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-04-28 23:07:28 -0400

    Update Main.java
    - Added docs to randomNumbers()
    - Added params to randomNumbers()

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,25 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Sort sort = new Sort();
-        int[] sorted = sort.mergeSort(randomNumbers());
+        int[] sorted = sort.mergeSort(randomNumbers(1000, 17));
 
         for (int num : sorted) {
             System.out.println(num);
         }
     }
-    private static int[] randomNumbers() {
+    
+    /**
+     * A simple method that provides random numbers.
+     * @param amount how many numbers do you want?
+     * @param bound the upper bound (but not including) of each number.
+     * @return an array of ints of given length.
+     */
+    private static int[] randomNumbers(int amount, int bound) {
         Random rand = new Random();
-        int[] randomNumbers = new int[30000];
+        int[] randomNumbers = new int[amount];
 
         for (int i = 0; i < randomNumbers.length; i++) {
-            randomNumbers[i] = rand.nextInt(1000);
+            randomNumbers[i] = rand.nextInt(bound);
         }
         return randomNumbers;
     }
